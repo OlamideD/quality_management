a4a2e2e21fb92f8517503835f4a13e82e378cedf
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-18 13:37:48 +0530

    test cases

--- quality_management/quality_management/doctype/quality_meeting/test_quality_meeting.py
+++ quality_management/quality_management/doctype/quality_meeting/test_quality_meeting.py
@@ -7,4 +7,23 @@ import frappe
 import unittest
 
 class TestQualityMeeting(unittest.TestCase):
-	pass
+	
+	def test_meeting(self):
+		createmeeting = create_meeting()
+		print("createmeeting")
+		getmeeting = get_meeting()
+		print("getmeeting")
+
+def create_meeting():
+	meeting = frappe.get_doc({
+		"doctype": "Quality Meeting",
+		"agenda": "Quality Meeting Test",
+		"scope": "Company",
+		"date": ""+ frappe.utils.nowdate() +""
+	})
+	meeting.insert()
+	return meeting
+
+def get_meeting():
+	meeting = frappe.get_all("Quality Meeting")
+	return meeting
--- quality_management/quality_management/doctype/quality_review/test_quality_review.py
+++ quality_management/quality_management/doctype/quality_review/test_quality_review.py
@@ -7,4 +7,28 @@ import frappe
 import unittest
 
 class TestPerformanceMonitoring(unittest.TestCase):
-	pass
+	
+	def test_review(self):
+		createreview = create_review_withaction()
+		print("createreview")
+
+def create_review_withaction():
+	for data in frappe.get_all("Quality Goal",fields=['name','frequency']):
+		if data.frequency == 'None':
+			print(data.frequency)
+			objectives = frappe.get_all("Quality Objective", filters={'parent': ''+ data.name +''}, fields=['objective', 'target', 'unit'])
+			review = frappe.get_doc({
+				"doctype": "Quality Review",
+				"goal": data.name,
+				"date": ""+ frappe.utils.nowdate() +"",
+			})
+			for objective in objectives:
+				review.append("values",{
+					'objective': objective.objective,
+					'target': objective.target,
+					'target_unit': objective.unit,
+					'achieved': "2",
+					'achieved_unit': objective.unit
+				})
+			review.insert()
+			return review
