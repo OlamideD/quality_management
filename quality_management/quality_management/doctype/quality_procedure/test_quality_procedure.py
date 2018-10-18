b972680d4ff20d3475ddeda83216b234040aaed0
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-18 21:39:46 +0530

    unit test bug fix

--- quality_management/quality_management/doctype/quality_procedure/test_quality_procedure.py
+++ quality_management/quality_management/doctype/quality_procedure/test_quality_procedure.py
@@ -8,38 +8,39 @@ import unittest
 
 class TestQualityProcedure(unittest.TestCase):
 	
-	def test_quality_management(self):
+	def test_quality_management(self):		
+		test_create_procedure = create_procedure()
+		test_get_procedure = get_procedure()
+		self.assertEquals(test_create_procedure.name, test_get_procedure.name)
+
+		test_create_goal = create_goal()
+		test_get_goal = get_goal()
+		self.assertEquals(test_create_goal.name, test_get_goal.name)
 		
-		test_procedure = procedure()
-		test_goal = goal()
-		test_unit = unit()
+		test_create_unit = create_unit()
+		test_get_unit = get_unit()
+		self.assertEquals(test_create_unit.name, test_get_unit.name)
+
 		test_create_review = create_review()
 		test_get_review = get_review()
+		self.assertEquals(test_create_review.name, test_get_review.name)
+		
 		test_action = get_action()
-		test_meeting = meeting()
+		self.assertEquals(test_action.review, test_create_review.name)
+		
+		test_create_meeting = create_meeting()
+		test_get_meeting = get_meeting()
+		self.assertEquals(test_create_meeting.name, test_get_meeting.name)
+
 		test_create_feedback = create_feedback()
 		test_get_feedback = get_feedback()
-		test_create_template = create_template()
-		test_get_template = get_template()
-
-		#print(test_procedure.name)
-		self.assertEquals(test_procedure.name, 'Quality Procedure Test')
-		#print(test_goal.name)
-		self.assertEquals(test_goal.name, 'Quality Goal Test')
-		#print(test_unit.name)
-		self.assertEquals(test_unit.name, 'Test Unit')
-		#print(test_create_review.name)
-		#print(test_get_review.name)
-		self.assertEquals(test_create_review.name, test_get_review.name)
-		#print(test_action.review)
-		self.assertEquals(test_action.review, test_create_review.name)
-		self.assertEquals(test_meeting.date, frappe.utils.nowdate())
-		#print(test_get_template.template)
-		self.assertEquals(test_get_template.template, 'Template Test')
-		
 		self.assertEquals(test_create_feedback.name, test_get_feedback.name)
 
-def procedure():
+		test_create_template = create_template()
+		test_get_template = get_template()
+		self.assertEquals(test_get_template.name, test_create_template.name)
+
+def create_procedure():
 	procedure = frappe.get_doc({
 		"doctype": "Quality Procedure",
 		"procedure": "Quality Procedure Test",
@@ -52,7 +53,11 @@ def procedure():
 	procedure.insert()
 	return procedure
 
-def goal():
+def get_procedure():
+	procedure = frappe.get_list("Quality Procedure")
+	return procedure[0]
+
+def create_goal():
 	goal = frappe.get_doc({
 		"doctype": "Quality Goal",
 		"goal": "Quality Goal Test",
@@ -68,13 +73,21 @@ def goal():
 	goal.insert()
 	return goal
 
-def unit():
+def get_goal():
+	goal = frappe.get_list("Quality Goal")
+	return goal[0]
+
+def create_unit():
 	unit = frappe.get_doc({
 		"doctype": "Measurement Unit",
 		"unit": "Test Unit"
 	})
 	unit.insert()
 	return unit
+
+def get_unit():
+	unit = frappe.get_list("Measurement Unit")
+	return unit[0]
 
 def create_review():
 	for data in frappe.get_all("Quality Goal",fields=['name','frequency']):
@@ -104,14 +117,19 @@ def get_action():
 	action = frappe.get_list("Quality Action", fields=["name","review"])
 	return action[0]
 
-def meeting():
+def create_meeting():
 	meeting = frappe.get_doc({
 		"doctype": "Quality Meeting",
 		"scope": "Company",
+		"status": "Close",
 		"date": ""+ frappe.utils.nowdate() +""
 	})
 	meeting.insert()
 	return meeting
+
+def get_meeting():
+	meeting = frappe.get_list("Quality Meeting", fields=['name', 'status'])
+	return meeting[0]
 
 def create_feedback():
 	feedback = frappe.get_doc({
@@ -122,7 +140,7 @@ def create_feedback():
 	return feedback
 
 def get_feedback():
-	feedback = frappe.get_list("Customer Feedback")
+	feedback = frappe.get_list("Customer Feedback", fields=['name', 'date'])
 	return feedback[0]
 
 def create_template():
