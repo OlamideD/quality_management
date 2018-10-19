dd55b1ed7f72d4eee7551fc3ba3ac491829bbdc7
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-19 14:33:41 +0530

    gfxghjjhdg

--- quality_management/quality_management/doctype/quality_review/quality_review.js
+++ quality_management/quality_management/doctype/quality_review/quality_review.js
@@ -11,26 +11,31 @@ frappe.ui.form.on('Quality Review', {
 		}
 	},
 	goal: function(frm) {
-		if (frm.doc.values != null){
-			frm.fields_dict.values.grid.remove_all()
-			frm.refresh();
-		}
-		frappe.call({
-            "method": "frappe.client.get",
-            args: {
-                doctype: "Quality Goal",
-				name: frm.doc.goal
-            },
-            callback: function (data) {
-				for (var i = 0; i < data.message.objective.length; i++ ){
-					frm.add_child("values");
-					frm.fields_dict.values.get_value()[i].objective = data.message.objective[i].objective;
-					frm.fields_dict.values.get_value()[i].target = data.message.objective[i].target;
-					frm.fields_dict.values.get_value()[i].target_unit = data.message.objective[i].unit;
-					frm.fields_dict.values.get_value()[i].achieved_unit = data.message.objective[i].unit;
-				}
+		if (frm.doc.goal != null){
+			if (frm.doc.values != null){
+				frm.fields_dict.values.grid.remove_all()
 				frm.refresh();
-            }
-        })
+			}
+			frappe.call({
+				"method": "frappe.client.get",
+				args: {
+					doctype: "Quality Goal",
+					name: frm.doc.goal
+				},
+				callback: function (data) {
+					for (var i = 0; i < data.message.objective.length; i++ ){
+						frm.add_child("values");
+						frm.fields_dict.values.get_value()[i].objective = data.message.objective[i].objective;
+						frm.fields_dict.values.get_value()[i].target = data.message.objective[i].target;
+						frm.fields_dict.values.get_value()[i].target_unit = data.message.objective[i].unit;
+						frm.fields_dict.values.get_value()[i].achieved_unit = data.message.objective[i].unit;
+					}
+					frm.refresh();
+				}
+			})
+		}
+		else{
+			
+		}
 	},
 });
--- quality_management/quality_management/doctype/quality_review/quality_review.py
+++ quality_management/quality_management/doctype/quality_review/quality_review.py
@@ -9,51 +9,27 @@ from frappe.model.document import Document
 class QualityReview(Document):	
 
 	def validate(self):
-		problem = ''
-		for value in self.values:
-			if int(value.achieved) < int(value.target):
-				problem = 'set'
-				break
+		if self.goal:
+			problem = ''
+			for value in self.values:
+				if int(value.achieved) < int(value.target):
+					problem = 'set'
+					break
 
-		if problem == 'set':
-			self.action = 'Action Initialised'
-		else:
-			self.action = 'No Action'
+			if problem == 'set':
+				self.action = 'Action Initialised'
+			else:
+				self.action = 'No Action'
 		
 	def after_insert(self):
-		problem = ''
-		for value in self.values:
-			if int(value.achieved) < int(value.target):
-				problem = problem + 'In '+ value.objective +', the Achieved Value '+ str(value.achieved) +' is less than the Target Value '+ str(value.target) +'\n'
+		if self.goal:
+			problem = ''
+			for value in self.values:
+				if int(value.achieved) < int(value.target):
+					problem = problem + 'In '+ value.objective +', the Achieved Value '+ str(value.achieved) +' is less than the Target Value '+ str(value.target) +'\n'
 
-		if(problem != ''):
-			problem = filter(None, problem.split("\n"))
-			doc = frappe.get_doc({
-				'doctype': 'Quality Action',
-				'action': 'Corrective',
-				'type': 'Quality Review',
-				'review': ''+ self.name +'',
-				'date': ''+ frappe.utils.nowdate() +'',
-				'procedure': ''+ self.procedure +''
-			})
-			for data in problem:
-				doc.append("description",{
-					'problem': data,
-					'status': 'Open'
-				})
-			doc.insert()
-			frappe.db.commit()
-
-	def on_update(self):
-		problem = ''
-		for value in self.values:
-			if int(value.achieved) < int(value.target):
-				problem = problem + 'In '+ value.objective +', the Achieved Value '+ str(value.achieved) +' is less than the Target Value '+ str(value.target) +'\n'
-
-		if problem != '':
-			problem = filter(None, problem.split("\n"))
-			query = frappe.get_list("Quality Action", filters={"review":""+ self.name +""})
-			if len(query) == 0:
+			if(problem != ''):
+				problem = filter(None, problem.split("\n"))
 				doc = frappe.get_doc({
 					'doctype': 'Quality Action',
 					'action': 'Corrective',
@@ -69,22 +45,49 @@ class QualityReview(Document):
 					})
 				doc.insert()
 				frappe.db.commit()
-			else:
-				child_table = frappe.get_list("Quality Action Table", filters={'parent': ''+ query[0].name +''})
-				for child in child_table:
-					frappe.delete_doc("Quality Action Table", ""+ child.name +"")
-				doc = frappe.get_doc("Quality Action", ""+ query[0].name +"")
-				for data in problem:
-					
-					doc.append("description",{
-						'problem': data,
-						'status': 'Open'
+
+	def on_update(self):
+		if self.goal:
+			problem = ''
+			for value in self.values:
+				if int(value.achieved) < int(value.target):
+					problem = problem + 'In '+ value.objective +', the Achieved Value '+ str(value.achieved) +' is less than the Target Value '+ str(value.target) +'\n'
+
+			if problem != '':
+				problem = filter(None, problem.split("\n"))
+				query = frappe.get_list("Quality Action", filters={"review":""+ self.name +""})
+				if len(query) == 0:
+					doc = frappe.get_doc({
+						'doctype': 'Quality Action',
+						'action': 'Corrective',
+						'type': 'Quality Review',
+						'review': ''+ self.name +'',
+						'date': ''+ frappe.utils.nowdate() +'',
+						'procedure': ''+ self.procedure +''
 					})
-				doc.save()
-				frappe.db.commit()
-		else:
-			query = frappe.get_list("Quality Action", filters={"review":""+ self.name +""})
-			if len(query) != 0:
-				frappe.delete_doc("Quality Action", ""+ query[0].name +"")
+					for data in problem:
+						doc.append("description",{
+							'problem': data,
+							'status': 'Open'
+						})
+					doc.insert()
+					frappe.db.commit()
+				else:
+					child_table = frappe.get_list("Quality Action Table", filters={'parent': ''+ query[0].name +''})
+					for child in child_table:
+						frappe.delete_doc("Quality Action Table", ""+ child.name +"")
+					doc = frappe.get_doc("Quality Action", ""+ query[0].name +"")
+					for data in problem:
+						
+						doc.append("description",{
+							'problem': data,
+							'status': 'Open'
+						})
+					doc.save()
+					frappe.db.commit()
 			else:
-				pass
+				query = frappe.get_list("Quality Action", filters={"review":""+ self.name +""})
+				if len(query) != 0:
+					frappe.delete_doc("Quality Action", ""+ query[0].name +"")
+				else:
+					pass
