1546e2aafabdb8167e3b23eed45f5d18656be1f7
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-22 12:15:48 +0530

    bug fix

--- quality_management/quality_management/doctype/quality_goal/quality_goal.js
+++ quality_management/quality_management/doctype/quality_goal/quality_goal.js
@@ -5,6 +5,18 @@ frappe.ui.form.on('Quality Goal', {
 	refresh: function(frm) {
 
 	},
+	onload: function(frm){
+		if(frm.doc.measurable == "No"){
+			frm.fields_dict.objective.grid.docfields[1].hidden = 1;
+			frm.fields_dict.objective.grid.docfields[2].hidden = 1;
+			frm.refresh();
+		}
+		else{
+			frm.fields_dict.objective.grid.docfields[1].hidden = 0;
+			frm.fields_dict.objective.grid.docfields[2].hidden = 0;
+			frm.refresh();
+		}
+	},
 	revision: function(frm) {
 		if(frm.doc.revised_on == null){
 			frm.set_value("revised_on", frappe.datetime.get_today());
