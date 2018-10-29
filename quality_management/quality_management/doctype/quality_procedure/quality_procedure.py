667ec595d7f6c7467e3f5d7584120c8880d4cce2
hrwX <himanshuwarekar@yahoo.com>
2018-10-29 16:12:33 +0530

    add treeview

--- quality_management/quality_management/doctype/quality_procedure/quality_procedure.py
+++ quality_management/quality_management/doctype/quality_procedure/quality_procedure.py
@@ -5,8 +5,10 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils.nestedset import NestedSet
 
-class QualityProcedure(Document):
+class QualityProcedure(NestedSet):
+	nsm_parent_field = 'parent_quality_procedure'
 	
 	def before_save(self):
 		for data in self.procedure_step:
@@ -14,3 +16,32 @@ class QualityProcedure(Document):
 				data.step = data.procedure_name
 			else:
 				pass
+
+@frappe.whitelist()
+def get_children(doctype, parent=None, parent_quality_procedure=None, is_root=False):
+	if parent == None or parent == "All Quality Procedures":
+		parent = ""
+
+	return frappe.db.sql("""
+		select
+			name as value,
+			is_group as expandable
+		from
+			`tab{doctype}` comp
+		where
+			ifnull(parent_quality_procedure, "")={parent}
+		""".format(
+			doctype = doctype,
+			parent=frappe.db.escape(parent)
+		), as_dict=1)
+
+@frappe.whitelist()
+def add_node():
+	from frappe.desk.treeview import make_tree_args
+	args = frappe.form_dict
+	args = make_tree_args(**args)
+
+	if args.parent_quality_procedure == 'All Quality Procedures':
+		args.parent_quality_procedure = None
+
+	frappe.get_doc(args).insert()
