05218f3b6fdda40923d4c4ab01b03b4a3574b421
hrwX <himanshuwarekar@yahoo.com>
2018-10-31 09:22:54 +0530

    Quality Action Unit Test

--- quality_management/quality_management/doctype/quality_action/test_quality_action.py
+++ quality_management/quality_management/doctype/quality_action/test_quality_action.py
@@ -11,10 +11,21 @@ test_dependencies = ["Quality Procedure","Measurement Unit" ,"Quality Goal", "Qu
 class TestQualityAction(unittest.TestCase):
 
 	def test_quality_action(self):
-		test_review, test_action = get_action()
-		self.assertEquals(test_review.name, test_action.review)
+		test_create_action = create_action()
+		test_get_action = get_action()
+		self.assertEquals(test_create_action.name, test_get_action.name)
+
+def create_action():
+	action = frappe.get_doc({
+		#add here
+	})
+	action_exist = frappe.get_list("Quality Action", filters={"goal": "_Test Quality Goal"})
+	if len(action_exist) == 0:
+		action.insert()
+		return action
+	else:
+		return action_exist[0]
 
 def get_action():
-	review = frappe.get_list("Quality Review", fields=["name"])
-	action = frappe.get_list("Quality Action", filters={"review": ""+ review[0].name +""}, fields=["name" ,"review"])
-	return review[0], action[0]
+	action = frappe.get_list("Quality Action")
+	return action[0]
